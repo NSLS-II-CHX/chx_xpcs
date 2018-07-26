05c794795c2fa99f266713e048d2ff4ceae9348b
Julien Lhermitte <jrmlhermitte@gmail.com>
2018-07-25 23:49:41 -0400

    more flake8

--- chx_compress/io/multifile/conversion.py
+++ chx_compress/io/multifile/conversion.py
@@ -1,6 +1,7 @@
-from .multifile import MultifileAPS, MultifileBNL
+from .multifile import MultifileAPS, MultifileBNL # noqa
 
-def multifileBNL2multifileAPS(fileame_in, filename_out):
+
+def multifileBNL2multifileAPS(filename_in, filename_out):
     '''
        Convert an APS multifile to a BNL multifile
 
@@ -12,18 +13,19 @@ def multifileBNL2multifileAPS(fileame_in, filename_out):
        filename_out : str
             the name of the output filename
     '''
-
     fin = MultifileAPS(filename_in)
     # get the md
-    header = fin._read_header(0)
+    # need to complete this later
+    header = fin._read_header(0)  # noqa
 
-    #fout = MultifileBNL(filenam_out, md=md)
+    # fout = MultifileBNL(filenam_out, md=md)
 
     '''
     for i in range(fin.Nframes):
         pos, vals = fin.rdrawframe(i)
         fout._write_raw(pos, vals)
     '''
+
 
 def APS2BNLheader(header):
     # convert from APS to BNL header dict
